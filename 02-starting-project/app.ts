7fa98677e0850962354ab52ef03a699147140ff8
Chachchai Tongthavornsuwan <38347860+CartoonToYou@users.noreply.github.com>
2023-03-01 23:16:01 +0700

    :book: Learn function type

--- 02-starting-project/app.ts
+++ 02-starting-project/app.ts
@@ -37,30 +37,68 @@
 //   console.log('is admin');
 // }
 
-type Combinable = number | string;
-type ConversionDescriptor = 'as-number' | 'as-text'
+// type Combinable = number | string;
+// type ConversionDescriptor = 'as-number' | 'as-text'
 
-function combine(input1: Combinable, input2: Combinable, resultConversion: ConversionDescriptor) {
-  let result;
-  if(typeof input1 ==='number' && typeof input2 === 'number' || resultConversion === 'as-number'){
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result
+// function combine(input1: Combinable, input2: Combinable, resultConversion: ConversionDescriptor) {
+//   let result;
+//   if(typeof input1 ==='number' && typeof input2 === 'number' || resultConversion === 'as-number'){
+//     result = +input1 + +input2;
+//   } else {
+//     result = input1.toString() + input2.toString();
+//   }
+//   return result
 
-  // if(resultConversion === 'as-number') {
-  //   return +result;
-  // } else {
-  //   return result.toString();
-  // }
+//   // if(resultConversion === 'as-number') {
+//   //   return +result;
+//   // } else {
+//   //   return result.toString();
+//   // }
+// }
+
+// const combineAges = combine(30, 26, 'as-number');
+// console.log(combineAges)
+
+// const combineStringAges = combine('30', '26', 'as-number')
+// console.log(combineStringAges)
+
+// const combineName = combine('Toon', 'Tong', 'as-text')
+// console.log(combineName)
+
+function add(n1:number, n2:number) {
+  return n1+n2;
 }
 
-const combineAges = combine(30, 26, 'as-number');
-console.log(combineAges)
+/* Return something with type function(): type */
+/* Don't return nothing or produce nothing from function use "void" */
+function printResult(num:number): void {
+  console.log('Result: '+ num);
+}
 
-const combineStringAges = combine('30', '26', 'as-number')
-console.log(combineStringAges)
+function addAndHandle(n1:number, n2:number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
 
-const combineName = combine('Toon', 'Tong', 'as-text')
-console.log(combineName)
+printResult(add(5,12))
+
+/* Plain function */
+// let combineValues: Function;
+
+/* Function that no recieve any params and return number */
+// let combineValues: (a: number, b: number) => number;
+
+/* Function that recieve 2 params and return number */
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+// combineValues = printResult;
+/* assign new value and call it that not function will error when runtime */
+// combineValues = 5
+
+console.log(combineValues(8,10));
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+  return result;
+})
