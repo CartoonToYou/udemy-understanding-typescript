a1866b472f573ec184ab1133f0495e09b23fa845
Chachchai Tongthavornsuwan <chachchai_toon@hotmail.com>
2023-03-01 16:23:40 +0700

    :book: Learn many types of ts

--- 02-starting-project/app.ts
+++ 02-starting-project/app.ts
@@ -1,9 +1,66 @@
-function add(n1: number,n2: number) {
-  return n1+n2
+// const person: {name: string; age: number; hobbies: string[]; role: [number, string]} = {
+//   name: 'Chachchai',
+//   age: 30,
+//   hobbies: ["Running", 'Reading'],
+//   role: [2,'author']
+// }
+
+// const ADMIN = 0
+// const READ_ONLY = 1
+// const AUTHOR = 2
+
+// enum Role {ADMIN = 'ADMIN', READ_ONLY = 100, AUTHOR = 200}
+
+// const person = {
+//   name: 'Chachchai',
+//   age: 30,
+//   hobbies: ["Running", 'Reading'],
+//   role: Role.ADMIN
+// }
+
+// // person.role.push('admin');
+// // person.role[1] = 10;
+
+// // person.role = [0, 'admin', 'user']
+
+// let favoriteActivity: string[];
+// favoriteActivity = ['Toon']
+
+// console.log(person.name)
+
+// for(const hobby of person.hobbies) {
+//   console.log(hobby.toLocaleUpperCase());
+//   // console.log(hobby.map()) !!! ERROR !!!
+// }
+
+// if(person.role === Role.ADMIN) {
+//   console.log('is admin');
+// }
+
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text'
+
+function combine(input1: Combinable, input2: Combinable, resultConversion: ConversionDescriptor) {
+  let result;
+  if(typeof input1 ==='number' && typeof input2 === 'number' || resultConversion === 'as-number'){
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result
+
+  // if(resultConversion === 'as-number') {
+  //   return +result;
+  // } else {
+  //   return result.toString();
+  // }
 }
 
-const number1 = 5;
-const number2 = 2.8;
+const combineAges = combine(30, 26, 'as-number');
+console.log(combineAges)
 
-const result = add(+number1, number2);
-console.log(result);
+const combineStringAges = combine('30', '26', 'as-number')
+console.log(combineStringAges)
+
+const combineName = combine('Toon', 'Tong', 'as-text')
+console.log(combineName)
